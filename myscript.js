0024500659d028b7fa9f123a1d3928bff7975bb2
Forhad Reza <75982069+F-Reza@users.noreply.github.com>
2021-04-04 00:13:10 +0600

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,17 @@
+/*---------------------
+Authorized::Forhad Reza
+-----------*/
+
+function displayNum(n){
+	cal.txt.value+=n;
+}
+
+function numBack(){
+	cal.txt.value=cal.txt.value.slice(0,-1);
+}
+function numClear(){
+	cal.txt.value="";
+}
+function squerRoot(){
+	cal.txt.value=Math.sqrt(cal.txt.value);
+}
